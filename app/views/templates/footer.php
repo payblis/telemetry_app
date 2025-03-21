2f4e650aa861b14c91bd8ff93e4238372860b077
payblis <info@payblis.com>
2025-03-21 10:10:48 +0100

    fix

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -35,5 +35,67 @@
         });
     });
     </script>
+
+    <!-- jQuery -->
+    <script src="vendors/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- FastClick -->
+    <script src="vendors/fastclick/lib/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="vendors/nprogress/nprogress.js"></script>
+    <!-- Chart.js -->
+    <script src="vendors/Chart.js/dist/Chart.min.js"></script>
+    <!-- jQuery Sparklines -->
+    <script src="vendors/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+    <!-- Flot -->
+    <script src="vendors/Flot/jquery.flot.js"></script>
+    <script src="vendors/Flot/jquery.flot.time.js"></script>
+    <script src="vendors/Flot/jquery.flot.resize.js"></script>
+    <!-- DateJS -->
+    <script src="vendors/DateJS/build/date.js"></script>
+    <!-- Custom Theme Scripts -->
+    <script src="js/custom.min.js"></script>
+
+    <script>
+    // Initialisation des composants
+    $(document).ready(function() {
+        // Gestion des menus déroulants
+        $('.dropdown-toggle').dropdown();
+
+        // Gestion des tooltips
+        $('[data-toggle="tooltip"]').tooltip();
+
+        // Animation des cartes
+        $('.fade-in').each(function(index) {
+            $(this).delay(100 * index).animate({opacity: 1}, 500);
+        });
+
+        // Gestion du menu mobile
+        $('#menu_toggle').on('click', function() {
+            if ($('body').hasClass('nav-md')) {
+                $('body').removeClass('nav-md').addClass('nav-sm');
+            } else {
+                $('body').removeClass('nav-sm').addClass('nav-md');
+            }
+        });
+
+        // Notifications temps réel (à implémenter)
+        function checkNotifications() {
+            $.get('index.php?route=notifications/check', function(data) {
+                if (data.count > 0) {
+                    $('.info-number .badge').text(data.count).show();
+                }
+            });
+        }
+        // setInterval(checkNotifications, 60000); // Vérification toutes les minutes
+    });
+    </script>
+
+    <?php if (isset($pageSpecificScripts)): ?>
+        <?php foreach ($pageSpecificScripts as $script): ?>
+            <script src="<?php echo $script; ?>"></script>
+        <?php endforeach; ?>
+    <?php endif; ?>
 </body>
 </html> 
