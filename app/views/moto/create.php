98dda89f8f9ab31e0582757d2796a17b3c0f71cb
payblis <info@payblis.com>
2025-03-21 11:24:16 +0100

    Update create.php

--- app/views/moto/create.php
+++ app/views/moto/create.php
@@ -1,5 +1,61 @@
 <?php require_once APP_PATH . 'views/templates/header.php'; ?>
 
+<style>
+    .x_panel {
+        margin-bottom: 20px;
+        border: 1px solid #e6e9ed;
+        border-radius: 4px;
+        box-shadow: 0 1px 3px rgba(0,0,0,0.12);
+    }
+    .x_title {
+        border-bottom: 2px solid #e6e9ed;
+        padding: 1px 5px 6px;
+        margin-bottom: 10px;
+        background: #f7f7f7;
+    }
+    .x_title h2 {
+        font-size: 18px;
+        color: #2A3F54;
+    }
+    .x_content {
+        padding: 15px;
+    }
+    .form-group {
+        margin-bottom: 15px;
+    }
+    .control-label {
+        padding-top: 7px;
+        font-weight: bold;
+    }
+    .required {
+        color: red;
+    }
+    .form-control {
+        border-radius: 4px;
+    }
+    .form-control:focus {
+        border-color: #66afe9;
+        box-shadow: 0 0 8px rgba(102,175,233,.6);
+    }
+    .checkbox {
+        margin-top: 0;
+        margin-bottom: 10px;
+    }
+    .btn {
+        margin-right: 10px;
+    }
+    textarea.form-control {
+        min-height: 80px;
+    }
+    h4 {
+        color: #2A3F54;
+        margin-top: 20px;
+        margin-bottom: 15px;
+        padding-bottom: 5px;
+        border-bottom: 1px solid #e6e9ed;
+    }
+</style>
+
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-12">
@@ -21,33 +77,33 @@
                                 <div class="form-group">
                                     <label class="control-label col-md-3">Marque <span class="required">*</span></label>
                                     <div class="col-md-6">
-                                        <input type="text" name="marque" required class="form-control">
+                                        <input type="text" name="marque" required class="form-control" value="Honda">
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <label class="control-label col-md-3">Modèle <span class="required">*</span></label>
                                     <div class="col-md-6">
-                                        <input type="text" name="modele" required class="form-control">
+                                        <input type="text" name="modele" required class="form-control" value="CBR1000RR-R">
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <label class="control-label col-md-3">Année <span class="required">*</span></label>
                                     <div class="col-md-6">
-                                        <input type="number" name="annee" required class="form-control" min="1900" max="2100">
+                                        <input type="number" name="annee" required class="form-control" value="2024">
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <label class="control-label col-md-3">Cylindrée (cc) <span class="required">*</span></label>
                                     <div class="col-md-6">
-                                        <input type="number" name="cylindree" required class="form-control">
+                                        <input type="number" name="cylindree" required class="form-control" value="1000">
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <label class="control-label col-md-3">Type <span class="required">*</span></label>
                                     <div class="col-md-6">
                                         <select name="type_moto" required class="form-control">
+                                            <option value="Superbike" selected>Superbike</option>
                                             <option value="MotoGP">MotoGP</option>
-                                            <option value="Superbike">Superbike</option>
                                             <option value="Supersport">Supersport</option>
                                             <option value="Endurance">Endurance</option>
                                         </select>
@@ -56,25 +112,25 @@
                                 <div class="form-group">
                                     <label class="control-label col-md-3">Puissance (ch) <span class="required">*</span></label>
                                     <div class="col-md-6">
-                                        <input type="number" name="puissance_moteur" required class="form-control">
+                                        <input type="number" name="puissance_moteur" required class="form-control" value="215">
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <label class="control-label col-md-3">Couple (Nm)</label>
                                     <div class="col-md-6">
-                                        <input type="number" step="0.1" name="couple_moteur" class="form-control">
+                                        <input type="number" step="0.1" name="couple_moteur" class="form-control" value="112">
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <label class="control-label col-md-3">Poids à sec (kg)</label>
                                     <div class="col-md-6">
-                                        <input type="number" step="0.1" name="poids_sec" class="form-control">
+                                        <input type="number" step="0.1" name="poids_sec" class="form-control" value="168">
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <label class="control-label col-md-3">Poids en ordre de marche (kg)</label>
                                     <div class="col-md-6">
-                                        <input type="number" step="0.1" name="poids_ordre_marche" class="form-control">
+                                        <input type="number" step="0.1" name="poids_ordre_marche" class="form-control" value="201">
                                     </div>
                                 </div>
                             </div>
@@ -91,19 +147,19 @@
                                 <div class="form-group">
                                     <label class="control-label col-md-3">Marque <span class="required">*</span></label>
                                     <div class="col-md-6">
-                                        <input type="text" name="fourche_marque" required class="form-control">
+                                        <input type="text" name="fourche_marque" required class="form-control" value="Öhlins">
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <label class="control-label col-md-3">Modèle</label>
                                     <div class="col-md-6">
-                                        <input type="text" name="fourche_modele" class="form-control">
+                                        <input type="text" name="fourche_modele" class="form-control" value="NPX-EVO">
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <label class="control-label col-md-3">Précharge (tours)</label>
                                     <div class="col-md-6">
-                                        <input type="number" step="0.5" name="fourche_precharge" class="form-control">
+                                        <input type="number" step="0.5" name="fourche_precharge" class="form-control" value="2.5">
                                     </div>
                                 </div>
                                 <div class="form-group">
@@ -123,13 +179,13 @@
                                 <div class="form-group">
                                     <label class="control-label col-md-3">Marque <span class="required">*</span></label>
                                     <div class="col-md-6">
-                                        <input type="text" name="amortisseur_marque" required class="form-control">
+                                        <input type="text" name="amortisseur_marque" required class="form-control" value="Öhlins">
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <label class="control-label col-md-3">Modèle</label>
                                     <div class="col-md-6">
-                                        <input type="text" name="amortisseur_modele" class="form-control">
+                                        <input type="text" name="amortisseur_modele" class="form-control" value="TTX36">
                                     </div>
                                 </div>
                                 <div class="form-group">
@@ -175,13 +231,13 @@
                                 <div class="form-group">
                                     <label class="control-label col-md-3">Étrier avant - Marque <span class="required">*</span></label>
                                     <div class="col-md-6">
-                                        <input type="text" name="etrier_avant_marque" required class="form-control">
+                                        <input type="text" name="etrier_avant_marque" required class="form-control" value="Brembo">
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <label class="control-label col-md-3">Étrier avant - Modèle</label>
                                     <div class="col-md-6">
-                                        <input type="text" name="etrier_avant_modele" class="form-control">
+                                        <input type="text" name="etrier_avant_modele" class="form-control" value="Stylema">
                                     </div>
                                 </div>
                                 <div class="form-group">
@@ -227,21 +283,20 @@
                                 <div class="form-group">
                                     <label class="control-label col-md-3">Couronne (dents) <span class="required">*</span></label>
                                     <div class="col-md-6">
-                                        <input type="number" name="couronne_dents" required class="form-control">
+                                        <input type="number" name="couronne_dents" required class="form-control" value="42">
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <label class="control-label col-md-3">Pignon (dents) <span class="required">*</span></label>
                                     <div class="col-md-6">
-                                        <input type="number" name="pignon_dents" required class="form-control">
+                                        <input type="number" name="pignon_dents" required class="form-control" value="16">
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <label class="control-label col-md-3">Type chaîne</label>
                                     <div class="col-md-6">
                                         <select name="chaine_type" class="form-control">
-                                            <option value="">Sélectionner</option>
-                                            <option value="520">520</option>
+                                            <option value="520" selected>520</option>
                                             <option value="525">525</option>
                                             <option value="530">530</option>
                                         </select>
@@ -278,7 +333,7 @@
                                 <div class="form-group">
                                     <label class="control-label col-md-3">Marque <span class="required">*</span></label>
                                     <div class="col-md-6">
-                                        <input type="text" name="echappement_marque" required class="form-control">
+                                        <input type="text" name="echappement_marque" required class="form-control" value="Akrapovic">
                                     </div>
                                 </div>
                                 <div class="form-group">
@@ -291,8 +346,7 @@
                                     <label class="control-label col-md-3">Type</label>
                                     <div class="col-md-6">
                                         <select name="echappement_type" class="form-control">
-                                            <option value="">Sélectionner</option>
-                                            <option value="ligne_complete">Ligne complète</option>
+                                            <option value="ligne_complete" selected>Ligne complète</option>
                                             <option value="silencieux_seul">Silencieux seul</option>
                                         </select>
                                     </div>
@@ -316,7 +370,7 @@
                                 <div class="form-group">
                                     <label class="control-label col-md-3">ECU Marque <span class="required">*</span></label>
                                     <div class="col-md-6">
-                                        <input type="text" name="ecu_marque" required class="form-control">
+                                        <input type="text" name="ecu_marque" required class="form-control" value="HRC">
                                     </div>
                                 </div>
                                 <div class="form-group">
@@ -330,12 +384,12 @@
                                     <div class="col-md-6">
                                         <div class="checkbox">
                                             <label>
-                                                <input type="checkbox" name="capteur_vitesse"> Vitesse
+                                                <input type="checkbox" name="capteur_vitesse" checked> Vitesse
                                             </label>
                                         </div>
                                         <div class="checkbox">
                                             <label>
-                                                <input type="checkbox" name="capteur_regime"> Régime moteur
+                                                <input type="checkbox" name="capteur_regime" checked> Régime moteur
                                             </label>
                                         </div>
                                         <div class="checkbox">
@@ -385,7 +439,7 @@
                                 <div class="form-group">
                                     <label class="control-label col-md-3">Marque <span class="required">*</span></label>
                                     <div class="col-md-6">
-                                        <input type="text" name="pneu_marque" required class="form-control">
+                                        <input type="text" name="pneu_marque" required class="form-control" value="Pirelli">
                                     </div>
                                 </div>
                                 <div class="form-group">
@@ -398,8 +452,7 @@
                                     <label class="control-label col-md-3">Type gomme</label>
                                     <div class="col-md-6">
                                         <select name="type_gomme" class="form-control">
-                                            <option value="">Sélectionner</option>
-                                            <option value="Soft">Soft</option>
+                                            <option value="Soft" selected>Soft</option>
                                             <option value="Medium">Medium</option>
                                             <option value="Hard">Hard</option>
                                             <option value="Rain">Rain</option>
@@ -438,10 +491,9 @@
                                     <label class="control-label col-md-3">Type guidon</label>
                                     <div class="col-md-6">
                                         <select name="type_guidon" class="form-control">
-                                            <option value="">Sélectionner</option>
+                                            <option value="demi-guidon" selected>Demi-guidon</option>
                                             <option value="standard">Standard</option>
                                             <option value="racing">Racing</option>
-                                            <option value="demi-guidon">Demi-guidon</option>
                                         </select>
                                     </div>
                                 </div>
