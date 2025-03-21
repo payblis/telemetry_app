bc2314c719c412f37f8f1f4c9aba255b9b709448
payblis <info@payblis.com>
2025-03-21 09:59:41 +0100

    fix

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -6,7 +6,7 @@
     <title><?php echo isset($pageTitle) ? $pageTitle : 'Télémétrie IA'; ?></title>
     
     <!-- CSS -->
-    <link rel="stylesheet" href="/public/css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <!-- Bootstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
@@ -15,33 +15,33 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="/">Télémétrie IA</a>
+            <a class="navbar-brand" href="index.php">Télémétrie IA</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" href="/">Accueil</a>
+                        <a class="nav-link" href="index.php">Accueil</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/telemetrie">Télémétrie</a>
+                        <a class="nav-link" href="index.php?route=telemetrie">Télémétrie</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/reglages">Réglages</a>
+                        <a class="nav-link" href="index.php?route=reglages">Réglages</a>
                     </li>
                 </ul>
                 <ul class="navbar-nav ms-auto">
                     <?php if(isset($_SESSION['user_id'])): ?>
                         <li class="nav-item">
-                            <a class="nav-link" href="/profil">Mon Profil</a>
+                            <a class="nav-link" href="index.php?route=profil">Mon Profil</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/logout">Déconnexion</a>
+                            <a class="nav-link" href="index.php?route=logout">Déconnexion</a>
                         </li>
                     <?php else: ?>
                         <li class="nav-item">
-                            <a class="nav-link" href="/login">Connexion</a>
+                            <a class="nav-link" href="index.php?route=login">Connexion</a>
                         </li>
                     <?php endif; ?>
                 </ul>
